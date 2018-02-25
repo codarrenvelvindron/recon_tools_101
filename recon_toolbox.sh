57f3a54e3b0535e1822d68020847fd2832c4fdfe
codarrenvelvindron <codarren@hackers.mu>
2018-02-25 15:48:45 +0300

    oops

--- recon_toolbox.sh
+++ recon_toolbox.sh
@@ -6,5 +6,5 @@ read domain
 mkdir -p ./output/$domain
 #./Sublist3r/sublist3r.py -d $domain -o ./subdomains/input_$domain.txt
 ./recon_dom_cleanup/recon_dom_cleanup.py -d $domain
-#./recon_dom_title/recon_dom_title.py -d $domain
-#./recon_dom_server/recon_dom_server.py -d $domain
+./recon_dom_title/recon_dom_title.py -d $domain
+./recon_dom_server/recon_dom_server.py -d $domain
