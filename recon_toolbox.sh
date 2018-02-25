6970a4db1bb923a1945f03271e9c9043320adab0
codarrenvelvindron <codarren@hackers.mu>
2018-02-25 16:07:27 +0300

    fix commented lines

--- recon_toolbox.sh
+++ recon_toolbox.sh
@@ -4,7 +4,7 @@
 echo "Check which domain?(e.g: example.com)"
 read domain
 mkdir -p ./output/$domain
-#./Sublist3r/sublist3r.py -d $domain -o ./subdomains/input_$domain.txt
+./Sublist3r/sublist3r.py -d $domain -o ./subdomains/input_$domain.txt
 ./recon_dom_cleanup/recon_dom_cleanup.py -d $domain
 ./recon_dom_title/recon_dom_title.py -d $domain
 ./recon_dom_server/recon_dom_server.py -d $domain
