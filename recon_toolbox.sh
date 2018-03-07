17b97b80f5043b1eae8ce2870a270ec7a6ce09dd
codarrenvelvindron <codarren@hackers.mu>
2018-03-07 11:56:31 +0300

    new

--- recon_toolbox.sh
+++ recon_toolbox.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# recon_toolbox
+# version 1.3
+
 echo "Check which domain?(e.g: example.com)"
 read domain
+mkdir -p ./output/$domain
 ./Sublist3r/sublist3r.py -d $domain -o ./subdomains/input_$domain.txt
 ./recon_dom_cleanup/recon_dom_cleanup.py -d $domain
 ./recon_dom_title/recon_dom_title.py -d $domain
 ./recon_dom_server/recon_dom_server.py -d $domain
-./recon_dom_resolve/recon_dom_resolve.py -d $domain
